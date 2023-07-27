6824c5c198f3549ca52cb1cb801ed4e8809f8ce3
inioluwabello <inioluwabellodavid@gmail.com>
2023-07-27 10:26:17 +0100

    initia;

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const { connectToDatabase } = require("./db");
+
+require("dotenv").config();
+
+app.use(express.json());
+
+app.use("/test", require("./routes/test"));
+
+const startServer = async () => {
+  await connectToDatabase();
+  
+  app.listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
+};
+
+startServer();
